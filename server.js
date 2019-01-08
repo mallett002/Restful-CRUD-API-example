a183820b2d8efc2cad62e96e8ee921ea56ea175e
William Mallett <MallettWilliam@JohnDeere.com>
2019-01-08 07:10:56 -0600

    connecting to database

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // connecting to the database
-
+mongoose.connect(config.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+})
 
 // default route
 app.get('/', (req, res) => {
